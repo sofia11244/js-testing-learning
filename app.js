f485927cb0c4f667c395b1e24753836ff52714be
sofia11244 <denizsofia03@gmail.com>
2024-07-27 21:18:39 +0300

    cahnges made

---
+++ app.js
@@ -0,0 +1,35 @@
+// variables example in a game  health bar every hit gonna minus to your health bar
+// variable is a container that stores a value. *sanki içerdiği değermiş gibi davranır
+
+
+// declaration = let x;
+// assignment = x = 100; 
+// let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+
+// console.log(`You are ${age} years old`);
+console.log(`price is ${price}`)
+
+// string
+let firstName = "Bro";
+console.log(typeof firstName);
+console.log(`your first name is ${firstName}`)
+
+// booleans true/false
+let online = false;
+console.log(`${firstName} is online:${online}`)
+
+
+let fullName = "Slay";
+let age = 25;
+let confirm = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}.`
+document.getElementById("p2").textContent = `Your age is ${age}.`
+document.getElementById("p3").textContent = `blah blah is ${confirm}.`
+
+
+
+
